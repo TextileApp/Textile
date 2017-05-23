89953e5fb4bef0524be5d17c7fd24ede5a8b7189
Matthew Bowman <matthew.bowman55@gmail.com>
2017-05-22 19:46:21 -0700

    fixed outfits page
    fixed outfits page

--- src/pages/outfits/outfits.ts
+++ src/pages/outfits/outfits.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, NgZone} from '@angular/core';
-import { NavController,ToastController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import {settingsPage} from '../settings/settings';
 import * as firebase from 'firebase';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
@@ -19,31 +19,15 @@ myUser: any;
 af: AngularFire;
 isEnabled: boolean;
 userName: any;
+
 tempUsername: string;
-  constructor(public navCtrl: NavController,private ngZone: NgZone,af: AngularFire,private _auth: AuthService,public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController,private ngZone: NgZone,af: AngularFire,private _auth: AuthService,) {
     const authObserver = af.auth.subscribe( user => {
   if (user) {
-      var username;
-var tempUsername;
-    this.myUser = user.uid;
-     tempUsername = user.auth.email;
-
-var ref = firebase.database().ref(user.uid+'/username');
-ref.once('value', function(snapshot) {
-  if (snapshot.val() === null) {
-   
-    
-    username = tempUsername.substr(0, tempUsername.indexOf('@'));
-     var db = firebase.database().ref(user.uid+'/username');
-
-db.set(
- username
-);
-  } else {
-    // username already exists, ask user for a different name
-  }
-});
+ this.myUser = user.uid;
   this.outfits = af.database.list(this.myUser+'/outfits/');
+ 
+  this.update();
   } 
   this.isEnabled = false;
 
@@ -52,70 +36,31 @@ db.set(
   
    ngAfterViewInit() {
    }
-/**
-this.mySlideOptions = {
-    loop: true,
+
+update()
+{
+var ref = firebase.database().ref(this.myUser+'/username');
+ref.once('value', (snapshot) => {
+
+ if (snapshot.val() === null) {
    
-    onSlideChangeEnd: s => {
-        //this.onDidChange();
-    },
-   
-    effect: 'coverflow',
-    centeredSlides: true,
-    slidesPerView: 1,
-    coverflow: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 4,
-        slideShadows: false
-    }
-};
+          var username;
+var tempUsername;
+    username = tempUsername.substr(0, tempUsername.indexOf('@'));
+     var db = firebase.database().ref(this.myUser+'/username');
 
+db.set(
+ username
+);
+  
+  }else{
+   this.userName = snapshot.val();
+console.log(snapshot.val());
   }
-  
-    ngOnInit() {
-      firebase.auth().onAuthStateChanged((_currentUser) => {
-
-      this.ngZone.run(() => {
-        if (_currentUser) {
-          console.log("in auth subscribe", _currentUser)
-         this.myUser = _currentUser.uid;
-
-          this.loadData();
-
-        } else {
-         this.myUser = null;
-        }
-
-      });
-
-    })
-  }
-  
-loadData() {
-    var result1 = [];
-
-firebase.database().ref(this.myUser+'/outfits/').on('child_added', function(data) {
-var element = data.val();
- element.key = data.key;
-result1.push(element);
-
 });
-
-firebase.database().ref(this.myUser+'/outfits/').on('child_removed', function(data) {
-  var element = data.val();
- var index = this.outfits.indexOf(element);
- if (index > -1) {
-    this.outfits.splice(index, 1);
-}
-});
-  //this.grid = Array(Math.ceil(this.items.length/2));
-  
-
   
 }
- */
+
 isDeleteEnabled()
 {
 return this.isEnabled;
